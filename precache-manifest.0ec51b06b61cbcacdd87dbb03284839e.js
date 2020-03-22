ad5027e9b47aaf1f874558da5d072c44f241b65a
Ghanem Jamal Eddine <ghanem97@hotmail.com>
2020-03-22 00:01:41 -0400

    Updates

---
+++ precache-manifest.0ec51b06b61cbcacdd87dbb03284839e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6dedd2d9e148f938fce",
+    "url": "/Portfolio/static/css/main.ca8b1503.chunk.css"
+  },
+  {
+    "revision": "f6dedd2d9e148f938fce",
+    "url": "/Portfolio/static/js/main.8eb144f2.chunk.js"
+  },
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.8760695a.js"
+  },
+  {
+    "revision": "702e430a218389b2ceea",
+    "url": "/Portfolio/static/js/2.1721d663.chunk.js"
+  },
+  {
+    "revision": "0b4e5fc0a7cab390a9f26509e2d44b40",
+    "url": "/Portfolio/index.html"
+  }
+];
